f7eb78520c84e6ff72824cc3f60efcab71b98d4c
iamyahia <yahiabaiz@gmail.com>
2025-04-13 11:56:41 +0300

    feat: refactor UserPanel to accept users as props and update Dashboard loader

--- app/components/user-panel.tsx
+++ app/components/user-panel.tsx
@@ -1,36 +1,44 @@
 import React from "react";
+import { Form } from "react-router";
 
-const contacts = [
-  { id: 1, fullName: "John Doe", avatar: "https://i.pravatar.cc/40?img=1" },
-  { id: 2, fullName: "Jane Smith", avatar: "https://i.pravatar.cc/40?img=2" },
-  {
-    id: 3,
-    fullName: "Alice Johnson",
-    avatar: "https://i.pravatar.cc/40?img=3",
-  },
-];
-
-const UserPanel: React.FC = () => {
+export default function UserPanel({
+  users,
+}: {
+  users: {
+    firstName: string;
+    lastName: string;
+  }[];
+}) {
   return (
-    <div className="w-full max-w-sm p-4 bg-white shadow rounded h-screen overflow-y-auto">
-      <h2 className="text-xl font-bold mb-4">Contact List</h2>
-      <div>
-        {contacts.map((contact) => (
-          <div
-            key={contact.id}
-            className="flex items-center p-2 hover:bg-gray-100 rounded"
-          >
-            <img
-              src={contact.avatar}
-              alt={contact.fullName}
-              className="w-10 h-10 rounded-full mr-3"
-            />
-            <span className="text-gray-700">{contact.fullName}</span>
-          </div>
-        ))}
+    <div className="relative w-full flex flex-col justify-between max-w-sm p-4 bg-white shadow rounded h-screen overflow-y-auto">
+      <div className="flex flex-col">
+        <h2 className="text-xl font-bold mb-4">Contact List</h2>
+        <div>
+          {users.map((user, index) => (
+            <div
+              key={index}
+              className="flex items-center p-2 hover:bg-gray-100 rounded"
+            >
+              <img
+                src={`https://i.pravatar.cc/40?img=${index}`}
+                alt={user.firstName + user.lastName}
+                className="w-10 h-10 rounded-full mr-3"
+              />
+              <span className="text-gray-700">
+                {user.firstName} {user.lastName}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
+      <Form method="post" action="/logout">
+        <button
+          type="submit"
+          className="absolute bottom-4 left-4 right-4 cursor-pointer bg-red-500 hover:bg-red-700 text-white py-2 px-4 rounded shadow"
+        >
+          Sign Out
+        </button>
+      </Form>
     </div>
   );
-};
-
-export default UserPanel;
+}
--- app/routes/_layout_._auth.dashboard.tsx
+++ app/routes/_layout_._auth.dashboard.tsx
@@ -1,10 +1,15 @@
 import UserPanel from "~/components/user-panel";
 import type { Route } from "./+types/_layout_._auth.dashboard";
+import type { LoaderFunctionArgs } from "react-router";
+import { getOtherUsers } from "~/utils/users.server";
 
-export default function Dashboard({ loaderData }: Route.ComponentProps) {
+export const loader = async ({ request }: LoaderFunctionArgs) =>
+  await getOtherUsers(request);
+
+export default function Dashboard({ loaderData: users }: Route.ComponentProps) {
   return (
     <>
-      <UserPanel />
+      <UserPanel users={users} />
     </>
   );
 }
