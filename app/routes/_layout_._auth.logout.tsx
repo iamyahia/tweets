3933e1b0357ac1232120326fbcbf0c111cab2ffa
iamyahia <yahiabaiz@gmail.com>
2025-04-13 16:25:55 +0300

    add: redirect user if try to go to logout route to home screen

--- app/routes/_layout_._auth.logout.tsx
+++ app/routes/_layout_._auth.logout.tsx
@@ -1,6 +1,8 @@
-import type { ActionFunctionArgs } from "react-router";
+import { redirect, type ActionFunctionArgs } from "react-router";
 
 import { logout } from "~/utils/session.server";
 
 export const action = async ({ request }: ActionFunctionArgs) =>
   logout(request);
+
+export const loader = async () => redirect("/");
