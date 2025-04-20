e755486372f6f9e4a3f376bb4ecc9c24ee970bb4
iamyahia <yahiabaiz@gmail.com>
2025-04-20 16:18:31 +0300

    feat: add color and emoji constants for consistent styling

---
+++ app/utils/constants.ts
@@ -0,0 +1,23 @@
+export const colorMap = {
+  RED: "text-red-400",
+  WHITE: "text-white",
+  BLUE: "text-blue-400",
+  GREEN: "text-green-400",
+  YELLOW: "text-yellow-400",
+};
+
+export const backgroundColorMap = {
+  RED: "bg-red-400",
+  WHITE: "bg-white",
+  BLUE: "bg-blue-400",
+  GREEN: "bg-green-400",
+  YELLOW: "bg-yellow-400",
+};
+
+export const emojiMap = {
+  SMILE: "😊",
+  SAD: "😢",
+  ANGRY: "😡",
+  LOVE: "😍",
+  THUMBS_UP: "👍",
+};
